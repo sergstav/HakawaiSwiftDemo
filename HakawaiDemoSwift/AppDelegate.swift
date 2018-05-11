cd5b843a3039954733c8fb6bdf45d8489cb14873
Serhii Stavytskyi <serg.stavitckii@gmail.com>
2018-05-11 23:15:34 +0300

    Swift example.

--- HakawaiDemoSwift/AppDelegate.swift
+++ HakawaiDemoSwift/AppDelegate.swift
@@ -2,8 +2,12 @@
 //  AppDelegate.swift
 //  HakawaiDemoSwift
 //
-//  Created by Serg Stavitskii on 5/11/18.
-//  Copyright © 2018 com.linkedin. All rights reserved.
+//  Copyright (c) 2014 LinkedIn Corp. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+//  the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //
 
 import UIKit
---
+++ Pods/Target Support Files/Pods-HakawaiDemoSwift/Pods-HakawaiDemoSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HakawaiDemoSwift : NSObject
+@end
+@implementation PodsDummy_Pods_HakawaiDemoSwift
+@end
